7e99a712f7986ac7e36fb398e0c855b31417e829
yliu22520 <yliu22520@gmail.com>
2026-02-17 05:37:00 +0800

    "chore:eslint_config"

---
+++ cloud-govern-ui/.eslintrc.cjs
@@ -0,0 +1,27 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
+  ignorePatterns: [
+    'dist',
+    'node_modules',
+    '*.d.ts',
+    'src/auto-imports.d.ts',
+    'src/components.d.ts',
+  ],
+}
--- cloud-govern-ui/vite.config.ts
+++ cloud-govern-ui/vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
-import AutoImport from 'unplugin-auto-import/vite';
-import Components from 'unplugin-vue-components/vite';
-import { TDesignResolver } from 'unplugin-vue-components/resolvers';
 
 export default defineConfig({
   plugins: [
